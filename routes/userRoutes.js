9c4ddb2576fe12c49a444fbde0cce21914cf1ce1
TRINH.NCK <trinh.nck@gmail.com>
2017-08-08 00:19:44 +0700

    Add user api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,2 +1,13 @@
 module.exports = function(app) {
+
+  var user = require('../controllers/userController');
+  
+  app.route('/categories')
+    .get(user.list_all_users)
+    .post(user.create_a_user);
+
+  app.route('/tasks/:userId')
+    .get(user.read_a_user)
+    .put(user.update_a_user)
+    .delete(user.delete_a_user);
 };
