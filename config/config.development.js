9ad8e87d19d4363858ad285e08c8fc154c37133d
Nguyễn Cảnh Khánh Trình <trinh.nck@gmail.com>
2017-09-21 03:18:39 +0700

    Delete config.development.js

--- config/config.development.js
+++
@@ -1,6 +0,0 @@
-var config = require('./config.global');
- 
-config.env = 'development';
-config.db = 'mongodb://trinhnck:password@ds127801.mlab.com:27801/testing_nodejs';
-
-module.exports = config;
